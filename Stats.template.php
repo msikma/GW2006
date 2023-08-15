a99368582b0f889aab000f0aeeabd4a2b9a69aa7
Michiel Sikma <michiel@sikma.org>
2023-08-15 13:26:15 +0200

    Template reorganization

--- Stats.template.php
+++ Stats.template.php
@@ -10,5 +10,5 @@ require_once('twig.php');
 // <http://vesuvius.local/gw/index.php?action=stats>
 // <http://vesuvius.local/gw/index.php?action=stats;collapse=202212#m202212>
 function template_main() {
-  return render_template('pages/general/forum_stats.twig');
+  return render_template('pages/general/forum_stats_center.twig');
 }
